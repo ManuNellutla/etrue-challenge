39ee9417ac2ff3a0b082af3d3621108a0e62ba0d
ManuNellutla <manu.nellutla@gmail.com>
2019-06-15 20:51:09 -0400

    Step 1: Shell project using Spring start io from intelliJ.

---
+++ src/main/java/com/etrue/challenge/CareerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.etrue.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerPortalApplication.class, args);
+    }
+
+}
